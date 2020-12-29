fab6b1cc50bfb9804351bf13a462306fe6090f32
Jon Keam <jpkeam@gmail.com>
2020-12-28 23:35:49 -0500

    add stimulusjs to handle dynamic changing of language

--- static/javascripts/main.js
+++ static/javascripts/main.js
@@ -1,22 +1,82 @@
-function Codebytes() {
-  var inputMirror;
-  var outputMirror;
+var inputMirror;
+var outputMirror;
 
-  this.submitCode= function() {
+window.onReady = function() {
+  const application = Stimulus.Application.start();
+  application.register('application', class extends Stimulus.Controller {
+    static get targets() {
+      return [ 'languageversion' ]
+    }
+
+    static get values() {
+      return { initiallanguageversion: String }
+    }
+
+    initialize() {
+      this.languageToSnippet = {
+        ruby: "puts 'hi'",
+        python: "print('hi')",
+        java: `public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}`,
+        erlang: `-module(hello).
+-export([hello_world/0]).
+
+hello_world() -> io:fwrite("hello, world").
+`
+      };
+      this.languageToMode = {
+        ruby: 'text/x-ruby',
+        python: 'text/x-python',
+        java: 'text/x-java',
+        erlang: 'text/x-erlang'
+      };
+
+      this.previousKey = this.keyFromLanguageVersion(this.initiallanguageversionValue);
+      this.resetInput(this.initiallanguageversionValue);
+    }
+
+    choose(e) {
+      e.preventDefault();
+      this.resetInput(e.target.text);
+    }
+
+    keyFromLanguageVersion(languageVersion){
+      return languageVersion.split(' ')[0].toLowerCase();
+    }
+
+    resetInput(chosen) {
+      this.languageversionTarget.innerHTML = chosen;
+      const key = this.keyFromLanguageVersion(chosen);
+      const doc = inputMirror.getDoc();
+
+      if (this.previousKey !== key) {
+        // set new example text
+        doc.setValue(this.languageToSnippet[key]);
+        // set new language
+        inputMirror.setOption('mode', this.languageToMode[key]);
+      }
+      this.previousKey = key;
+    }
+  });
+}
+
+function Codebytes() {
+  this.submitCode = function() {
     $('#disassemble_button').attr('disabled', 'disabled');
     outputMirror.setValue("Running...");
 
-    var rawText = inputMirror.getValue();
-
+    const rawText = inputMirror.getValue();
     if (!rawText) return;
-    var jsonText = JSON.stringify({"code":rawText});
 
     $.ajax({
       type: 'POST',
       url: '/',
       dataType: 'text',
       contentType: 'application/json; charset=utf-8',
-      data: jsonText,
+      data: JSON.stringify({"code":rawText}),
       complete: function(xhr, status) {
         $('#disassemble_button').removeAttr('disabled');
       },
@@ -30,9 +90,9 @@ function Codebytes() {
     });
   };
 
-  this.onReady= function() {
+  this.onReady = function() {
     inputMirror = CodeMirror.fromTextArea($("#inputTextArea").get(0), {lineNumbers:true});
     outputMirror = CodeMirror.fromTextArea($("#outputTextArea").get(0), {lineNumbers:true, readOnly :true});
   };
 }
-var codebytes = new Codebytes();
+window.codebytes = new Codebytes();
